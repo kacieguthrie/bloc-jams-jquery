b010e4d86857442a43bcab1505bb83ef19cad57b
Kacie Guthrie <kacieguthrie@gmail.com>
2018-05-03 22:32:52 -0700

    prev button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -3,12 +3,23 @@
     player.playPause();
     $(this).attr('playState', player.playState);
   });
+
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+    if (prevSongIndex < 0) { return; }
+
+    const prevSong = album.songs[prevSongIndex];
+    player.playPause(prevSong);
+  });
+
   $('button#next').on('click', function() {
     if (player.playState !== 'playing') { return; }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) {return;}
-    
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
